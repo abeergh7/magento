65f6e199c04d160640dcdc0ea3eff112c434e31a
abeergh7 <110747807+abeergh7@users.noreply.github.com>
2024-04-26 00:20:57 +0300

    the first four test cases

--- src/MyTestCases/SignUp.java
+++ src/MyTestCases/SignUp.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class SignUp extends parameters{
+public class SignUp extends parameters {
 	WebDriver driver = new ChromeDriver();
 
 	@BeforeTest
@@ -22,7 +22,7 @@ public class SignUp extends parameters{
 
 	}
 
-	@Test()
+	@Test(enabled = false)
 	public void myFirstTest() {
 
 		driver.get("https://magento.softwaretestingboard.com/");
@@ -34,19 +34,75 @@ public class SignUp extends parameters{
 		WebElement password = driver.findElement(By.id("password"));
 		WebElement confirmPassword = driver.findElement(By.id("password-confirmation"));
 		WebElement creatAccountButtonElement = driver.findElement(By.cssSelector("button[title='Create an Account']"));
-		//creatAccountButtonElement.click();
+		// creatAccountButtonElement.click();
 
 		firstName.sendKeys(firstNames[randomIndex]);
 		lastName.sendKeys(lastNames[randomIndex]);
 		Email.sendKeys(emailId);
 		password.sendKeys(commonpassword);
 		confirmPassword.sendKeys(commonpassword);
-				creatAccountButtonElement.click();
+		creatAccountButtonElement.click();
 
-		String welcomeMsg=driver.findElement(By.className("message-success")).getText();
-		
+		String welcomeMsg = driver.findElement(By.className("message-success")).getText();
 
-		assertEquals (welcomeMsg, "Thank you for registering with Main Website Store.");
+		assertEquals(welcomeMsg, "Thank you for registering with Main Website Store.");
 	}
 
+	@Test(priority = 2, enabled = false)
+	public void logoutTest() {
+
+		driver.get("https://magento.softwaretestingboard.com/customer/account/logout/\"");
+
+		assertEquals(driver.getCurrentUrl().contains("logoutSuccess"), true);
+
+	}
+
+	@Test(priority = 3, enabled = false)
+	public void loginProcess() throws InterruptedException {
+		driver.findElement(By.linkText("Sign In")).click();
+		driver.findElement(By.id("email")).sendKeys(emailId);
+		;
+		driver.findElement(By.id("pass")).sendKeys(commonpassword);
+		;
+		driver.findElement(By.id("send2")).click();
+
+		Thread.sleep(3000);
+		String welcomeMessage = driver.findElement(By.cssSelector(".greet.welcome")).getText();
+		assertEquals(welcomeMessage.contains("Welcome"), true);
+
+	}
+
+	@Test(priority = 4, enabled = true)
+	public void addRandomItem() throws InterruptedException {
+
+		driver.get("https://magento.softwaretestingboard.com/");
+//		WebElement footer= driver.findElement(By.cssSelector(".footer.content"));
+//		int theNumberOfATag=footer.findElements(By.tagName("a")).size();
+//		System.out.println(	theNumberOfATag);
+//		int theNumberOfH1Tag=driver.findElements(By.tagName("h1")).size();
+//		System.out.println(	theNumberOfH1Tag);
+//=================================================================
+		WebElement itemcontainer = driver.findElement(By.cssSelector(".product-items.widget-product-grid"));// orderlist
+		int numberofItems = itemcontainer.findElements(By.tagName("li")).size();
+		int randomItemToSelect = rand.nextInt(4);
+		System.out.println(randomItemToSelect);
+		itemcontainer.findElements(By.tagName("li")).get(randomItemToSelect).click();
+		Thread.sleep(2000);
+
+		WebElement sizeContainer = driver.findElement(By.cssSelector(".swatch-attribute.size"));
+		int theSizes = sizeContainer.findElements(By.cssSelector(".swatch-option")).size();
+		sizeContainer.findElements(By.cssSelector(".swatch-option")).get(rand.nextInt(theSizes)).click();
+		Thread.sleep(3000);
+
+		WebElement colorContainer = driver.findElement(By.cssSelector(".swatch-attribute.color"));
+		int theColor = colorContainer.findElements(By.cssSelector(".swatch-option")).size();
+		colorContainer.findElements(By.cssSelector(".swatch-option")).get(rand.nextInt(theColor)).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("product-addtocart-button")).click();
+		Thread.sleep(3000);
+
+		String ActualMsg = driver.findElement(By.className("messages")).getText();
+		assertEquals(ActualMsg.contains("You added"), true);
+		
+	}
 }
